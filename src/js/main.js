8049ab8179d1716a9ce4ea30c4504d0340cb187c
bastndev <gohit24@gmail.com>
2023-02-02 22:59:46 -0500

    Mode Dark

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click',()=>{
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+
+})
+
+// Music  https://www.youtube.com/watch?v=iU6FnSFY5X4
